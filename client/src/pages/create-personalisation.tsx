6f3a09b5a2e03303a3b33cfc0b2856856c25a5a4
nomad-coder420 <saketmahajan0@gmail.com>
2025-08-02 21:52:42 +0530

    fix

--- client/src/pages/create-personalisation.tsx
+++ client/src/pages/create-personalisation.tsx
@@ -1325,7 +1325,7 @@ export default function CreatePersonalisation() {
                      </div>
                    </div>
                 ) : (
-                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
                     {metrics.map((metric: any) => (
                                              <Card 
                          key={metric.pid}
